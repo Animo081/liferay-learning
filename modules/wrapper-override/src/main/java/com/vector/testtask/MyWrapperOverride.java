683b0ebca3ce019d518fc7331db3d38e21b907f9
Animo081 <xakerok27@mailru>
2019-10-11 11:03:47 +0300

    Yes

--- modules/wrapper-override/src/main/java/com/vector/testtask/MyWrapperOverride.java
+++ modules/wrapper-override/src/main/java/com/vector/testtask/MyWrapperOverride.java
@@ -9,7 +9,6 @@ import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceWrapper;
 import com.liferay.portal.kernel.service.UserLocalServiceWrapper;
-import jdk.nashorn.internal.ir.annotations.Reference;
 import org.osgi.service.component.annotations.Component;
 
 import javax.mail.internet.AddressException;
